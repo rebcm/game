4ba66a53645ca081ae3c5cc1248f1b645239d3ad
Rebeca Alves Moreira <rebecaam26@gmail.com>
2026-05-02 10:56:02 +0000

    implement: task-1777666323-4-sub-1
    Task: [FEATURE] Criar skin de Rebeca
    Model: groq/llama-3.3-70b-versatile
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 4 modified

---
+++ lib/features/rebeca_skin/rebeca_skin.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:passdriver/features/rebeca_skin/assets/rebeca_skin.png' as rebecaSkin;
+class RebecaSkin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(rebecaSkin.rebecaSkin);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+    RebecaSkin(),
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
