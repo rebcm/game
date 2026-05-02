e633c41ba8aad522b2b3c90aa6ddf6b15d96a00f
Rebeca Alves Moreira <rebecaam26@gmail.com>
2026-05-02 10:48:48 +0000

    implement: task-1777666323-4-sub-2
    Task: [FEATURE] Implementar Idle Animation
    Model: nvidia/llama-4-maverick
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 3 modified

---
+++ lib/features/driver_idle_animation/driver_idle_animation.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+class DriverIdleAnimation extends StatefulWidget {
+  @override
+  _DriverIdleAnimationState createState() => _DriverIdleAnimationState();
+}
+class _DriverIdleAnimationState extends State<DriverIdleAnimation> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 2))..repeat();
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _animationController.value * 0.1,
+          child: Image.asset('assets/images/rebeca.png'),
+        );
+      },
+    );
+  }
+}
---
+++ lib/features/driver_idle_animation/widgets/rebeca_idle.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:passdriver/features/driver_idle_animation/driver_idle_animation.dart';
+class RebecaIdle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DriverIdleAnimation();
+  }
+}
