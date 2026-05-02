716f77d4508653541e0b3573a430dbc3db82f7cf
Rebeca Alves Moreira <rebecaam26@gmail.com>
2026-05-02 18:12:57 +0000

    implement: task-1777666323-9-sub-3-disc-1777719929-2-disc-1777721170-2
    Task: [TEST] Implementar Teste de Timeout de Requisição
    Model: nvidia/llama-4-maverick
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 3 modified

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,20 +1,3 @@
-import 'package:flutter_driver/flutter_driver.dart';
-import 'package:test/test.dart';
+import 'package:integration_test/integration_test_driver.dart';
 
-void main() {
-  FlutterDriver? driver;
-
-  setUpAll(() async {
-    driver = await FlutterDriver.connect();
-  });
-
-  tearDownAll(() async {
-    if (driver != null) {
-      await driver?.close();
-    }
-  });
-
-  test('request timeout test', () async {
-    await driver?.waitFor(find.text('Timeout Error'));
-  });
-}
+Future<void> main() => integrationDriver();
