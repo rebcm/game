d501f7f343fc154676ccc05b2888f8ec9f862c29
Rebeca Alves Moreira <rebecaam26@gmail.com>
2026-05-02 12:26:03 +0000

    implement: task-1777666323-7-sub-8
    Task: [FEATURE] Configurar CORS para Flutter web
    Model: nvidia/llama-4-maverick
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 4 modified

---
+++ workers/api/handler.js
@@ -0,0 +1,22 @@
+async function handleRequest(request) {
+  const response = await fetch(request);
+  const newResponse = new Response(response.body, response);
+
+  newResponse.headers.set('Access-Control-Allow-Origin', '*');
+  newResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  newResponse.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
+  }
+
+  return newResponse;
+}
+
+export { handleRequest };
