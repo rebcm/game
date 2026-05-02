9ef1a7fac665618975b568bb1cea8c1aabbcb478
Rebeca Alves Moreira <rebecaam26@gmail.com>
2026-05-02 11:40:20 +0000

    implement: task-1777666323-4-sub-2-disc-1777720189-1
    Task: [DOCS] Definir Critérios de Aceitação da Animação Idle
    Model: nvidia/llama-4-maverick
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 3 modified

---
+++ lib/features/rebeca_animation/README.md
@@ -0,0 +1,4 @@
+RebecaAnimation:
+  - Duração do loop: 2000ms
+  - Amplitude do balanço: 0.1 radianos
+  - Easing function: Não aplicada (repetição linear)
---
+++ lib/features/rebeca_animation/widgets/rebeca_animation.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animation/simple_animation.dart';
+
+class RebecaAnimation extends StatefulWidget {
+  @override
+  _RebecaAnimationState createState() => _RebecaAnimationState();
+}
+
+class _RebecaAnimationState extends State<RebecaAnimation> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2000),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _animationController.value * 0.1,
+          child: child,
+        );
+      },
+      child: Image.asset('assets/rebeca.png'),
+    );
+  }
+}
