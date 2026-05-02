c59ca0cf055842b482f79db23b838df43869c1f4
Rebeca Alves Moreira <rebecaam26@gmail.com>
2026-05-02 11:08:10 +0000

    implement: task-1777666323-4-sub-1
    Task: [FEATURE] Criar skin de Rebeca
    Model: nvidia/llama-4-maverick
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 3 modified

---
+++ lib/features/rebeca_skin/lib/rebeca_skin.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RebecaSkin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/rebeca.png');
+  }
+}
---
+++ lib/features/rebeca_skin/lib/rebeca_skin_feature.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:passdriver/features/rebeca_skin/lib/rebeca_skin.dart';
+
+class RebecaSkinFeature extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RebecaSkin();
+  }
+}
