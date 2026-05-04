0601edd0c4b120e3a19b5be2fbed15a301b388b1
Rebeca AI <rebecaam26@gmail.com>
2026-05-04 06:39:13 +0000

    implement: task-1777666323-4-sub-4-disc-1777721760-3-disc-1777851839-3
    Task: [TEST] Validação de Atalhos Globais do Sistema
    Model: nvidia/llama-4-maverick
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 2 modified

--- test/integration_tests/global_shortcuts_test/global_shortcuts_driver.dart
+++ test/integration_tests/global_shortcuts_test/global_shortcuts_driver.dart
@@ -2,17 +2,9 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 void main() {
-  FlutterDriver? driver;
-
-  setUpAll(() async {
-    driver = await FlutterDriver.connect();
-  });
-
-  tearDownAll(() async {
-    driver?.close();
-  });
-
   test('global shortcuts test', () async {
-    await driver?.requestData('global_shortcuts_test');
+    final FlutterDriver driver = await FlutterDriver.connect();
+    await driver.requestData('global_shortcuts_test');
+    await driver.close();
   });
 }
