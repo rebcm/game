1d14b62c44da17ea62c024caddeaefe94df082f3
Rebeca AI <rebecaam26@gmail.com>
2026-05-04 00:05:52 +0000

    implement: task-1777666323-4-sub-4-disc-1777721760-1-disc-1777851644-1
    Task: [REFACTOR] Definir Enum de Ações do Jogador
    Model: nvidia/llama-4-maverick
    Implemented by Neuron AI via direct API in E2B sandbox.
    Files: 1 modified

--- test/enums/player_action_test.dart
+++ test/enums/player_action_test.dart
@@ -2,12 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:game/enums/player_action.dart';
 
 void main() {
-  test('PlayerAction enum values are correctly defined', () {
-    expect(PlayerAction.values.length, 5); // Update this count as you add more actions
-    expect(PlayerAction.moveForward.toString(), 'PlayerAction.moveForward');
-    expect(PlayerAction.moveBackward.toString(), 'PlayerAction.moveBackward');
-    expect(PlayerAction.moveLeft.toString(), 'PlayerAction.moveLeft');
-    expect(PlayerAction.moveRight.toString(), 'PlayerAction.moveRight');
-    expect(PlayerAction.jump.toString(), 'PlayerAction.jump');
+  test('PlayerAction enum values are correct', () {
+    expect(PlayerAction.values.length, 5); // Update the expected length as you add more actions
+    expect(PlayerAction.moveForward.index, 0);
+    expect(PlayerAction.moveBackward.index, 1);
+    expect(PlayerAction.moveLeft.index, 2);
+    expect(PlayerAction.moveRight.index, 3);
+    expect(PlayerAction.jump.index, 4);
   });
 }
